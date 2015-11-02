4b0c71e4fd7466ac6eb4aac46e0c9c886e7a873a
Aaron <aaronmyatt@gmail.com>
2015-11-02 20:17:03 +0800

    update gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -9,36 +9,33 @@ var protractorQA = require('gulp-protractor-qa');
      * Serve the Harp Site from the src directory
      */
     gulp.task('serve', function () {
-        harp.server("./static", {
+        harp.server("./static/", {
             port: 9000
         }, function () {
             browserSync({
+                browser: "google chrome canary",
                 proxy: "localhost:9000",
-                open: false,
+                // open: false
                 /* Hide the notification. It gets annoying */
                 notify: {
                     styles: ['opacity: 0', 'position: absolute']
                 }
             });
+            /**
+             * Watch for scss changes, tell BrowserSync to refresh main.css
+         */        
+            gulp.watch("*.sass", function () {
+                reload("./static/css/main.css", {stream: true});
+            });
+        
+            /**
+             * Watch all other changes, reload the whole page
+             */
+            gulp.watch(["./static/**/*.jade", "./static/**/*.js", "./static/**/*.json"], function () {
+                reload();
+            });
         });
-        gulp.watch("*.sass", function () {
-            reload("./static/css/main.css", {stream: true});
-        });
-        /**
-         * Watch for all other changes, reload the whole page
-         */
-        gulp.watch(["./static/**/*.jade", "./static./**/*.js"], function () {
-            reload();
-        });
-
-        /**
-         * Default task, running `gulp` will fire up the Harp site,
-         * launch BrowserSync & watch files.
-         */
     });
-    /**
-     * Watch for scss changes, tell BrowserSync to refresh main.css
-     */
 
     // e2e test notifcation https://github.com/ramonvictor/gulp-protractor-qa
     gulp.task('protractor-qa', function() {
