f93966c8b8a74f9ba5fac3faa6abb1ba37f6ae16
Aaron <aaronmyatt@gmail.com>
2016-01-27 00:38:51 +0800

    base tests

--- spec/e2e/index.spec.js
+++ spec/e2e/index.spec.js
@@ -1,41 +1,21 @@
-var index = require("./index_po.js");
+// ./spec/e2e/index.spec.js
 
-describe('angularjs homepage', function() {
-    it('Template page should display', function() {
+var index = require("./page_objects/index.po.js");
+var url = "http://127.0.0.1:3000";
+
+describe('index/base elements', function() {
+
+    beforeEach(function(){
         browser.ignoreSynchronization = true;
-        browser.get('http://127.0.0.1:3000');
-  });
+        browser.driver.get(url);
+        expect(browser.getLocationAbsUrl()).toBe("/view/upload");
+    });
+
+    describe('template basics', function() {
+        it("should find all elements", function() {
+            expect(index.directive.isPresent()).toBeTruthy();
+            index.checkMenu();
+            expect(index.uiView.isPresent()).toBeTruthy();
+        });
+    });
 });
-
-describe('base tempalate header', function() {
-  it("should be using a md-subheader element", function() {
-      index.header;
-  });
-});
-
-//   describe('todo list', function() {
-//     var todoList;
-
-//     beforeEach(function() {
-//       browser.get('http://www.angularjs.org');
-
-//       todoList = element.all(by.repeater('todo in todoList.todos'));
-//     });
-
-//     it('should list todos', function() {
-//       expect(todoList.count()).toEqual(2);
-//       expect(todoList.get(1).getText()).toEqual('build an angular app');
-//     });
-
-//     it('should add a todo', function() {
-//       var addTodo = element(by.model('todoList.todoText'));
-//       var addButton = element(by.css('[value="add"]'));
-
-//       addTodo.sendKeys('write a protractor test');
-//       addButton.click();
-
-//       expect(todoList.count()).toEqual(3);
-//       expect(todoList.get(2).getText()).toEqual('write a protractor test');
-//     });
-   // });
-// });
--- spec/e2e/page_objects/index.po.js
+++ spec/e2e/page_objects/index.po.js
@@ -1,5 +1,32 @@
+// ./spec/e2e/page_objects/upload.po.js
+
 var index = {
-    directive: element(by.tagName("index-directive"))
+    directive: element(by.tagName("gr-index-directive")),
+    menuButton: element(by.id("menuButton")),
+    uploadButton: element(by.id("uploadButton")),
+    infoButton: element(by.id("infoButton")),
+    uiView: element(by.tagName("ui-view")),
+    openMenu: openMenu,
+    linkTo: linkTo,
+    checkMenu: checkMenu
+};
+
+function openMenu(){
+    index.menuButton.click();
+}
+
+function linkTo(view){
+    var views = {
+        upload: index.uploadButton.click(),
+        info: index.infoButton.click()
+    }
+}
+
+function checkMenu(){
+    expect(index.menuButton.isPresent()).toBeTruthy();
+    index.openMenu();
+    expect(index.uploadButton.isPresent()).toBeTruthy();
+    expect(index.infoButton.isPresent()).toBeTruthy();
 }
 
 module.exports = index;
