8e4569da202bee27ede0e6d57f0152ccee57cbe7
Aaron <aaronmyatt@gmail.com>
2016-01-27 00:41:03 +0800

    starting on current-upload.service

--- denv/js/current-upload/current-upload.service.js
+++ denv/js/current-upload/current-upload.service.js
@@ -1,3 +1,36 @@
-/**
- * Created by aaron on 1/26/16.
- */
+// ./denv/js/current-upload/current-upload.service.js
+
+(function() {
+    'use strict';
+    angular
+        .module("current-upload.service", [])
+        .service("currentUploadService", currentUploadService);
+
+    currentUploadService.$inject = ["$log"];
+
+    function currentUploadService($log) {
+        // currentUploadService
+        // ------------
+
+        //Usage
+        //
+
+        var service = this;
+        var _upload = {};
+        service.updateUpload = updateUpload;
+        service.getUpload = getUpload;
+        
+        return service;
+
+        ////////////////
+        function updateUpload(data) {
+            $log.info("currentUploadService.updateUpload() called.");
+            _upload = data;
+        }
+
+        function getUpload(data) {
+            $log.info("currentUploadService.getUpload() called.");
+            return _upload;
+        }
+    }
+})();
--- denv/js/current-upload/current-upload.spec.js
+++ denv/js/current-upload/current-upload.spec.js
@@ -1,3 +1,27 @@
-/**
- * Created by aaron on 1/26/16.
- */
+// ./denv/js/current-upload/current-upload.spec.js
+
+var service;
+describe("current-upload.service", function (){
+    beforeEach(module("app"));
+    beforeEach(inject(function($injector){
+        service = $injector.get("currentUploadService");
+    }));
+
+    describe("updateUpload", function(){
+        it("sets the upload object", function(){
+            service.updateUpload({"title":"my resume"});
+            var upload = service.getUpload;
+            expect(upload().title).toEqual("my resume");
+        });
+    });
+
+    describe("getUpload", function(){
+       it("returns the uploaded json object", function(){
+           var upload = service.getUpload;
+           expect(upload()).toEqual({});
+           service.updateUpload({"title":"my resume"});
+           expect(upload().title).toEqual("my resume");
+       });
+    });
+});
+
